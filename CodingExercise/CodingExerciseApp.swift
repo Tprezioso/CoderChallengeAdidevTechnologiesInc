27f70c91576f0f9e5b6bbabfa1ef8bf060db65c1
Thomas <tommyprezioso@gmail.com>
2021-03-02 16:38:47 -0500

    Initial Commit

---
+++ CodingExercise/CodingExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingExerciseApp.swift
+//  CodingExercise
+//
+//  Created by Thomas Prezioso Jr on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CodingExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
